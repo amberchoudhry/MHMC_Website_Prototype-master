edc9de348d71011ee5f158a4c5416791df98850d
amberchoudhry <ac08221@sjfc.edu>
2018-02-20 03:42:00 -0500

    New Changes for presentation 2/20

---
+++ README.md
@@ -0,0 +1,1 @@
+# MHMC_Website_Prototype
---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>Home - MechaMagic</title>
+		
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Jquery --> 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	
+	<!-- Bootstrap must load first! -->
+	
+	<!-- Style CSS --> 
+    	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	
+	<!-- Montserrat Font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	
+  </head>
+  <body>
+      
+      
+      
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+              <a href="#">Story</a>
+              <a href="#">Follow Us</a>
+              <a href="#">Buy Now</a>
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+      
+      
+      
+	<div id="logo">
+	</div>
+	<div id="content">
+        <h2>Where Old Worlds meet New...</h2>
+        <img src="images/playButton.png" width="20%">
+        <video src="images/video.m4v" autoplay></video>
+	</div><!--/row-->
+	</div>
+	</div>
+	</div>
+	<footer class="footer">
+      <div class="container">
+        <p style="float:left">
+		<br>
+			Copyright 2018
+		</p>
+		<br>
+		<p style="float:right">
+			MechaMagic Game Studios
+		</p>
+      </div>
+    </footer>
+  </body>
+</html>
